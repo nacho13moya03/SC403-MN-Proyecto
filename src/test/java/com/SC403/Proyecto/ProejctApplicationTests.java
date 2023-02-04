88389978bbde7c9f48dd4cd7832dc19e4bd90495
nacho13moya03 <100731399+nacho13moya03@users.noreply.github.com>
2023-02-03 22:15:36 -0600

    Archivos Principales.
    Archivos principales para el proyecto.
    
    Estos archivos lo que contienen es el inicio del código para que lo pueda leer cualquier IDLE que quiera utilizar

---
+++ src/test/java/com/SC403/Proyecto/ProejctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SC403.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProejctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
