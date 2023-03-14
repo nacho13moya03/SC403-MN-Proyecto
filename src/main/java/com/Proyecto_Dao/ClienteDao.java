1179fa14335e63bb0be51be53c33bdbbd7bd5c0b
nacho13moya03 <ignaciomoya2003@gmail.com>
2023-03-14 06:25:19 -0600

    Actualizaciones

--- src/main/java/com/Proyecto_Dao/ClienteDao.java
+++ src/main/java/com/Proyecto_Dao/ClienteDao.java
@@ -4,10 +4,6 @@ package com.Proyecto_Dao;
 import com.Proyecto.Domain.Cliente;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- *
- * @author Adriana Ramirez
- */
-public interface ClienteDao extends CrudRepository<Cliente, Long>{
-    
+public interface ClienteDao extends CrudRepository<Cliente, Long> {
+
 }
