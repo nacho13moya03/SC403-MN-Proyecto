ca5681405bb1972894ada584aa6a5fd5a310bc07
nacho13moya03 <ignaciomoya2003@gmail.com>
2023-04-01 12:06:22 -0600

    Eliminacion de los CRUD

--- src/main/java/com/Proyecto_Dao/ClienteDao.java
+++
@@ -1,9 +0,0 @@
-
-package com.Proyecto_Dao;
-
-import com.Proyecto.Domain.Cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteDao extends CrudRepository<Cliente, Long> {
-
-}
--- src/main/java/com/SC403/ProjectApplication.java
+++ src/main/java/com/SC403/ProjectApplication.java
@@ -1,4 +1,4 @@
-package com.SC403.Proyecto;
+package com.SC403;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
