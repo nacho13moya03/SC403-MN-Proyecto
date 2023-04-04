f99947f19aa1d442041b18633caa5046d8c61e97
nacho13moya03 <ignaciomoya2003@gmail.com>
2023-04-03 23:01:52 -0600

    Cambios con el profe

---
+++ src/main/java/com/web/controller/CatalogueController.java
@@ -0,0 +1,22 @@
+package com.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/catalogue")
+public class CatalogueController {
+
+    //@Autowired
+    //ClienteService clienteService;
+
+        @GetMapping("/catalogs")
+        public String inicio(Model model) {
+
+
+            return "/catalogue/catalogs";
+        }
+
+    }
