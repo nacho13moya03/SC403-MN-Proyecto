a58114e35997c91c97a907626a2a050b502b7a19
nacho13moya03 <ignaciomoya2003@gmail.com>
2023-04-05 19:49:55 -0600

    Agregar DB

---
+++ src/main/java/com/web/domain/Catalogue.java
@@ -0,0 +1,46 @@
+package com.web.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "articulo")
+public class Catalogue implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_articulo")
+    private Long idArticulo;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private boolean activo;
+
+    @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria")
+    @ManyToOne
+    private Categorie categoria;
+
+    public Catalogue() {
+    }
+
+    public Catalogue(String descripcion, String detalle, double precio,
+                    int existencias, boolean activo, Categorie categorie) {
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.activo = activo;
+        this.categoria = categorie;
+    }
+}
